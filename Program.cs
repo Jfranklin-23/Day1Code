224586a5cb200adaa304d4505ed33623a37fb70e
franklin nieras <tcc.0182.2020@gmail.com>
2024-06-21 13:26:22 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Day1Code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "John Franklin";
+            string middleName = "Perez";
+            string lastName = "Nieras";
+            int age = 22;
+            string gender = "Male";
+
+            string fullName = ($"{firstName} {middleName} {lastName}");
+
+            System.Console.WriteLine($"My name is {fullName}. My gender is {gender}. I am {age} years old.");
+
+        }
+    }
+}
